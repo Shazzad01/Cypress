0e1be60dfac1f19294cb6b882dc8f83f19e942d2
Shazzad01 <shazzadm065@gmail.com>
2024-03-03 16:38:00 +0600

    03/03/2024

--- cypress/e2e/Basics/addToCart.cy.js
+++ cypress/e2e/Basics/addToCart.cy.js
@@ -6,7 +6,17 @@ beforeEach(function(){
 
 it('Add to cart', function(){
 
-    cy.title().should('eq','nopCommerce demo store1')
+    cy.title().should('eq','nopCommerce demo store')
+    cy.get('#small-searchterms').type('Apple MacBook Pro 13-inch')
+    cy.wait(5000)
+    cy.xpath("//button[normalize-space()='Search']").click()
+    cy.get('.product-title > a').click()
+    cy.wait(5000)
+    cy.get('#add-to-cart-button-4').click()
+    
+    cy.xpath("//a[normalize-space()='shopping cart']").click()
+    cy.get('.product-unit-price').contains("$1,800.00")
+    cy.get('.product-subtotal').contains("$3,600.00")
 })
 
 })
