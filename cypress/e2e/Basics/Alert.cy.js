1f9ad7fd70bc80a9c5ee50e0c4dd4183c935e534
Shazzad01 <shazzadm065@gmail.com>
2024-03-06 16:57:56 +0600

    03/06/2024

---
+++ cypress/e2e/Basics/Alert.cy.js
@@ -0,0 +1,38 @@
+describe('alert test', function(){
+
+beforeEach(function(){
+
+    cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+})
+
+let js_alert= "//button[normalize-space()='Click for JS Alert']"
+let js_confirm= "//button[normalize-space()='Click for JS Confirm']"
+let js_prompt="//button[normalize-space()='Click for JS Prompt']"
+let result = "//p[@id='result']"
+
+it('test',function(){
+
+    cy.xpath(js_alert).click()
+    cy.wait(3000)
+    cy.xpath(result).should('have.text','You successfully clicked an alert')
+    cy.wait(3000)
+
+
+
+    cy.on('window:confirm', function(){
+        return true
+    })
+
+    cy.xpath(js_confirm).click()
+    cy.wait(3000)
+
+    
+
+    cy.window().then(function($win){
+        cy.stub($win,'prompt').returns('Hello Prompt')
+        cy.xpath(js_prompt).click()
+    })
+
+})
+
+})
